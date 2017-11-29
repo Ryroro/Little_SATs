21d0f68b610be96d95223e92525c3f416139ac63
Qianyi Shu <qs4617@line18.doc.ic.ac.uk>
2017-11-29 19:33:59 +0000

    something

---
+++ C++/test/test_bcp.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void test_bcp_main() {
+  ;
+}
+
+void test_bcp_template(bool result) {
+
+  if (result) {
+    
+  } else {
+  }
+  
+
+void test_bcp1() {
+  Parser parser;
+  Formula formula;
+  vector<string> test_cases =
+    { "4 3 1 0 -1 2 0 -1 -2 3 0",
+      "4 3 1 0 -1 2 0 -2 1 3 0",
+      "1 0 1 0 1 0};
+
+  vector<string> test_results =
+    { "4 3 1 0 -1 2 0 -1 -2 3 0"
+  
+  auto begin = test_cases.begin();
+  auto end = test_cases.end();
+}
+
+
